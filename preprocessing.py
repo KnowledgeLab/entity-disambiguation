43eadbc3a220d602045ec6a90ff4b6316b6bd1da
bill10 <billanson10@gmail.com>
2019-06-04 13:10:59 -0400

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -36,4 +36,34 @@ with open("author_candidates.tsv","w") as outfile:
             outfile.write('\t{}'.format(v))
         outfile.write('\n')
     
-        
+with open("wos2id.tsv") as infile:
+    for l in infile:
+        i,j=l.split('\t')
+        wos2id[i]=int(j)
+
+refs=defaultdict(set)
+citations=defaultdict(set)
+with open("wos_refs.tsv") as infile:
+    next(infile)
+    for l in infile:
+        i,j=l.split('\t')
+        i=i.strip()
+        j=j.strip()
+        if i in wos2id:
+            if j in wos2id:
+                refs[wos2id[i]].add(wos2id[j])
+                citations[wos2id[j]].add(wos2id[i])
+
+with open("ref_list.tsv","w") as outfile:
+    for k in refs:
+        outfile.write('{}'.format(k))
+        for v in refs[k]:
+            outfile.write('\t{}'.format(v))
+        outfile.write('\n')  
+
+with open("citation_list.tsv","w") as outfile:
+    for k in citations:
+        outfile.write('{}'.format(k))
+        for v in citations[k]:
+            outfile.write('\t{}'.format(v))
+        outfile.write('\n')              
