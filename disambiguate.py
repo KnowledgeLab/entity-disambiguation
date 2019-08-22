fafc26651a8a375fd10284d0d74ff1d29dd6bc26
bill10 <billanson10@gmail.com>
2019-08-21 21:58:27 -0700

    remove networkx

--- disambiguate.py
+++ disambiguate.py
@@ -1,9 +1,12 @@
 import time
 import pickle
-import networkx as nx
+# import networkx as nx
 from itertools import combinations
 import numpy as np
 from multiprocessing import Pool, cpu_count
+from scipy.sparse import csr_matrix
+from scipy.sparse.csgraph import connected_components
+from collections import defaultdict
 
 class Stopwatch:
     start_time=None
@@ -51,22 +54,30 @@ def sx(i,j):
 def link(e):
     i,j=e
     y=sum([sa(i,j),sr(i,j),sc(i,j),sx(i,j)])
-    if y>0:
+    if y>1:
         return e
     else:
         return None
 
     
 def disambiguate(papers):
-    # iterate over all possible pairs and construct data
-    with Pool(2) as pool:
-        G = nx.Graph()
-        G.add_nodes_from(papers)
+    # iterate over all possible pairs and construct graph
+    with Pool(10) as pool:
+#         G = nx.Graph()
+#         G.add_nodes_from(papers)
+        G=[]
         for e in pool.imap_unordered(link, combinations(papers, 2), 100):
             if e is not None:
-                G.add_edges_from([e])
+#                 G.add_edges_from([e])
+                G.append(e)
     # export data of component
-    res = [(n,c) for n,c in enumerate(nx.connected_components(G))]
+#     res = [(n,c) for n,c in enumerate(nx.connected_components(G))]
+    G=csr_matrix((np.ones(len(G)), zip(*G)), shape=[len(papers)]*2)
+    n_components, labels = connected_components(csgraph=G, directed=False)
+    res=defaultdict(list)
+    for i,c in enumerate(labels):
+        res[c].append(i)
+    res=res.values()
     return res
 
 
@@ -97,26 +108,36 @@ with open('author_list.tsv') as f:
         A[line[0]] = set([i.lower() for i in line[1:]])
 tic.stop('{} rows. Elapsed'.format(len(A)))
 
-#author list
+#author candidates
 tic.go('Loading authors to be resolved...')
 sample=set()
 with open('disambiguate_candidates.txt') as f:
     for l in f:
-        sample.add(l.strip())
-    
+        sample.add(l.strip().lower())
+
+try:       
+    with open('disambiguated_authors.tsv') as infile:
+        for l in infile:
+            line=l.strip().split('\t')
+            name=line[0].split('_')[0]
+            sample.discard(name.lower())
+except:
+    pass
+        
 candidates={}
 with open('author_candidates_clean.tsv') as f:
     for l in f:
         line=l.strip().split('\t')
-        if line[0] in sample:
+        if line[0].lower() in sample:
             candidates[line[0]] = [int(i) for i in line[1:]]
 tic.stop('{} authors. Elapsed'.format(len(candidates)))
 
 # with open('AllEmailsTrainedGradientBoostingClassifier.sav', 'rb') as f:
 #     clt=pickle.load(f)
 
-with open('disambiguated_authors.tsv', 'w') as outfile:
-    for k in candidates:
+with open('disambiguated_authors.tsv', 'a+') as outfile:
+    for k in candidates[:5]:
+        print('Resolving {}'.format(k), flush=True)
         res=disambiguate(candidates[k])
         for i,c in res:
             outfile.write('{}_{}'.format(k,i))
